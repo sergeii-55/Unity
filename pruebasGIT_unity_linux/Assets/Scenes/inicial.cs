c95e57a4d4f24f20e4ff5327a61cdd1cbcbef2d6
sergeii <55vias55@gmail.com>
2018-12-12 11:44:11 -0700

    all the files
    Segundo commit del gitKraken

---
+++ pruebasGIT_unity_linux/Assets/Scenes/inicial.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inicial : MonoBehaviour {
+
+	// Use this for initialization
+	//este script es para probar el gitkraken de linux, en ausencia del github desktop
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
