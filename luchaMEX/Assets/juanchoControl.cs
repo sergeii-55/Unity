4599e040f4b725cf4a43f5d9fec0562fa7759251
sergeii <55vias55@gmail.com>
2019-01-09 09:29:32 -0700

    - folder de preparacion con archivos blender, texturas y de mas - cambios de level

--- luchaMEX/Assets/juanchoControl.cs
+++ luchaMEX/Assets/juanchoControl.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class juanchoControl : MonoBehaviour 
 {
-	float speed = 4;
-	float rotSpeed = 80;
-	float rot = 0f;
+//Debug.Log("Hello");
+	float speed = 3;
 	float gravity = 8;
 
 	Vector3 moveDir = Vector3.zero;
@@ -22,25 +21,116 @@ public class juanchoControl : MonoBehaviour
 
 	void Update()
 	{
-		if (controller.isGrounded)
-		{
+		Movement();
+		GetInput();
+	}
+
+	void Movement()
+	{
+		
+			/*    move right   */
+			if (Input.GetKey (KeyCode.D))
+			{
+				if(anim.GetBool("attacking")==true)
+				{
+					return;
+				}
+				else if(anim.GetBool("attacking")==false)
+				{
+					anim.SetBool ("running", true);
+					anim.SetInteger ("condition", 1);
+					transform.localRotation = Quaternion.Euler(0, 0, 0);
+					moveDir = new Vector3 (0, 0, 1);
+					moveDir *= speed;
+					moveDir = transform.TransformDirection (moveDir);
+				}
+			}
+
+			/*     move left       */
+			if (Input.GetKey (KeyCode.A))
+			{
+				if(anim.GetBool("attacking")==true)
+				{
+					return;
+				}
+				else if(anim.GetBool("attacking")==false)
+				{
+					anim.SetBool ("running", true);
+					anim.SetInteger ("condition", 1);
+					transform.localRotation = Quaternion.Euler(0, 180, 0);
+					moveDir = new Vector3 (0, 0, 1);
+					moveDir *= speed;
+					moveDir = transform.TransformDirection (moveDir);
+				}
+			}
+			/*      move up      */
 			if (Input.GetKey (KeyCode.W))
 			{
-				anim.SetInteger ("condition", 1);
-				moveDir = new Vector3 (0, 0, 1);
-				moveDir *= speed;
-				moveDir = transform.TransformDirection (moveDir);
+				if(anim.GetBool("attacking")==true)
+				{
+					return;
+				}
+				else if(anim.GetBool("attacking")==false)
+				{
+					anim.SetBool ("running", true);
+					anim.SetInteger ("condition", 1);
+					transform.localRotation = Quaternion.Euler(0, -90, 0);
+					moveDir = new Vector3 (0, 0, 1);
+					moveDir *= speed;
+					moveDir = transform.TransformDirection (moveDir);
+				}
 			}
-			if (Input.GetKeyUp (KeyCode.W))
+			/*      move down      */
+			if (Input.GetKey (KeyCode.S))
 			{
+				if(anim.GetBool("attacking")==true)
+				{
+					return;
+				}
+				else if(anim.GetBool("attacking")==false)
+				{
+					anim.SetBool ("running", true);
+					anim.SetInteger ("condition", 1);
+					transform.localRotation = Quaternion.Euler(0, 90, 0);
+					moveDir = new Vector3 (0, 0, 1);
+					moveDir *= speed;
+					moveDir = transform.TransformDirection (moveDir);
+				}
+			}
+			if (Input.GetKeyUp (KeyCode.W)||Input.GetKeyUp (KeyCode.A)||Input.GetKeyUp (KeyCode.S)||Input.GetKeyUp (KeyCode.D))
+			{
+				anim.SetBool ("running", false);
 				anim.SetInteger ("condition", 0);
 				moveDir = new Vector3 (0, 0, 0);
 			}
-		}
-		rot += Input.GetAxis ("Horizontal") * rotSpeed * Time.deltaTime;
-		transform.eulerAngles = new Vector3 (0, rot, 0);
-
 		moveDir.y -= gravity * Time.deltaTime;
 		controller.Move (moveDir * Time.deltaTime);
+	}
+	void GetInput()
+	{
+			if(Input.GetKey (KeyCode.Keypad5))
+			{
+				if(anim.GetBool("running")==true)
+				{
+					anim.SetBool ("running",false);
+					anim.SetInteger ("condition",0);
+				}
+				if(anim.GetBool("running")==false)
+				{
+					Attacking();
+				}
+			}
+	}
+	void Attacking()
+	{
+		anim.SetBool("attacking",true);
+		anim.SetInteger("condition",2);
+		StartCoroutine(AttackRoutine());
+	}
+	IEnumerator AttackRoutine()
+	{
+		yield return new WaitForSeconds(1);	
+		anim.SetInteger ("condition", 0);
+		anim.SetBool("attacking",false);
 	}
 }
